f71158fb59b63ec94aea366ecb0ee3b74656685a
AidanBaldeo <167706902+AidanBaldeo@users.noreply.github.com>
2024-04-21 14:02:44 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Restirate</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+	<body>
+		<?php include "nav.php"; ?>
+
+		<div id="hangingSignContainer">
+			<div id="hangingSign">
+				<div id="hangingSignContent">
+					<span>Welcome to Restirate!</span>
+					<span>This is a website that lets</span>
+					<span>you vote for your favorite restaurant</span>
+				</div>
+			</div>
+		</div>
+  </body>
+</html>
